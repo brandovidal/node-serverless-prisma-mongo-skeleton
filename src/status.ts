e5468c49af9f8abffb8545117b18eb030a557214
Brando Vidal <yisusdracon@gmail.com>
2023-01-31 19:17:05 -0500

    ⚒  using typescript and esbuild
    Signed-off-by: Brando Vidal <yisusdracon@gmail.com>

---
+++ src/status.ts
@@ -0,0 +1,17 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEvent, context?: Context): Promise<APIGatewayProxyResult> {
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ 
+      status: true, 
+      data: event.body, 
+      method: event.requestContext.httpMethod,
+      path: event.path, 
+      headers: event.headers, 
+      query: event.queryStringParameters
+    }),
+  };
+}
