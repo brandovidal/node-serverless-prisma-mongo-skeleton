18b2d9574f62a15da05ab37855e863764b9524f2
brando vidal <yisusdracon@gmail.com>
2023-02-02 18:11:37 -0500

    ⚒  using AWS System Manager
    Signed-off-by: brando vidal <yisusdracon@gmail.com>

--- app/status.ts
+++ app/status.ts
@@ -1,14 +1,15 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import type { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda'
 
 export async function handler(event: APIGatewayProxyEvent, context?: Context): Promise<APIGatewayProxyResult> {
-  const message = process.env.MESSAGE || 'Hello World!';
+  const message = process.env.MESSAGE || 'Hello World!'
+  const dbUrl = process.env.DATABASE_URL || 'Database!'
 
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ 
-      status: true, 
-      data: message
-    }),
-  };
+    body: JSON.stringify({
+      status: true,
+      data: { message, dbUrl }
+    })
+  }
 }
