b616b187f5d5d2082eb4e55cc91a57d2778d7e80
brando vidal <yisusdracon@gmail.com>
2023-02-01 19:45:42 -0500

    ⚒  using message variable
    Signed-off-by: brando vidal <yisusdracon@gmail.com>

--- app/status.ts
+++ app/status.ts
@@ -1,13 +1,14 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
 export async function handler(event: APIGatewayProxyEvent, context?: Context): Promise<APIGatewayProxyResult> {
+  const message = process.env.MESSAGE || 'Hello World!';
 
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ 
       status: true, 
-      data: 'Hello Service!'
+      data: message
     }),
   };
 }
